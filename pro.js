eb7bca7bede3ca61c6f60c8a2ba11b57b2da7258
bouchfar <h.zizou2000@gmail.com>
2021-12-17 15:20:09 +0100

    Create pro.js

---
+++ pro.js
@@ -0,0 +1,18 @@
+root@dm800se:
+wget https://raw.githubusercontent.com/biko-73/AjPanel/main/installer.sh -O - | /bin/sh
+
+--2021-12-12 08:34:35--  https://raw.githubusercontent.com/biko-73/AjPanel/main/installer.sh
+
+Resolving raw.githubusercontent.com... 185.199.111.133, 185.199.108.133, 185.199.110.133, ...
+Connecting to raw.githubusercontent.com|185.199.111.133|:443... connected.
+HTTP request sent, awaiting response... 200 OK
+Length: 6912 (6.8K) [text/plain]
+Saving to: â€˜STDOUTâ€™
+
+
+-                     0%[                    ]       0  --.-KB/s              
+***************************************************
+**              AJPanel Installation              *
+***************************************************
+
+Checking Server Version ...
